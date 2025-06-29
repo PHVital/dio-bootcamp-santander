2a68cc7fd5cd91caefc14a254f0686e9590338cd
PHVital <vital.ph.g@gmail.com>
2025-06-29 12:45:16 -0300

    feat: Aula sobre Records

--- POO/PrimeiraClasse/src/Main.java
+++ POO/PrimeiraClasse/src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        var male = new Person("João");
-        male.incAge();
-        male.incAge();
-        male.incAge();
-        var female = new Person("Maria");
-        System.out.println("Male name: " + male.getName() + " Age: " + male.getAge());
-        System.out.println("Female name: " + female.getName() + " Age: " + female.getAge());
+        var person = new Person("João", 21);
+        System.out.println(person);
+        System.out.println(person.name());
     }
 }
