e6f36b7b36b5c75d94ed1624a081db4e0a25a562
PHVital <vital.ph.g@gmail.com>
2025-07-03 15:24:21 -0300

    feat Adiciona aula sobre pesquisa em List

---
+++ Collections/List/Pesquisa/src/CatalogoLivros.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for (Livro livro : livroList) {
+                if (livro.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(livro);
+                }
+            }
+            return livrosPorAutor;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos (int anoInicial, int anoFinal) {
+        List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for (Livro livro : livroList) {
+                if (livro.getAnoPublicacao() >= anoInicial && livro.getAnoPublicacao() <= anoFinal) {
+                    livrosPorIntervaloAnos.add(livro);
+                }
+            }
+            return livrosPorIntervaloAnos;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public Livro pesquisarPorTitulo(String titulo) {
+        Livro livroPorTitulo = null;
+        if (!livroList.isEmpty()) {
+            for (Livro livro : livroList) {
+                if (livro.getTitulo().equalsIgnoreCase(titulo)) {
+                    livroPorTitulo = livro;
+                    break;
+                }
+            }
+            return livroPorTitulo;
+        } else {
+            throw new RuntimeException("A lista esta vazia!");
+        }
+    }
+
+}
