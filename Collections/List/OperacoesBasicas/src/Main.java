2af784137d566197a3011bd1348fc69610d9a02f
PHVital <vital.ph.g@gmail.com>
2025-07-03 14:58:06 -0300

    feat: Aula sobre a collections List

---
+++ Collections/List/OperacoesBasicas/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("O numero total de tarefas é " + listaTarefa.obterNumeroTotalTarefas());
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+
+        System.out.println("O numero total de tarefas é " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 2");
+
+        System.out.println("O numero total de tarefas é " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+    }
+}
