085ccaf9d9bd44557f4668a7f0d3a12b8a3b5c0a
PHVital <vital.ph.g@gmail.com>
2025-07-05 11:25:38 -0300

    feat: Adiciona aula sobre Ordenacao em Maps

---
+++ Collections/Map/Ordenacao/src/Livro.java
@@ -0,0 +1,49 @@
+import java.util.Comparator;
+import java.util.Map;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private double preco;
+
+    public Livro(String titulo, String autor, double preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
+
+class ComparatorPorPreco implements Comparator<Map.Entry<String, Livro>> {
+    @Override
+    public int compare(Map.Entry<String, Livro> o1, Map.Entry<String, Livro> o2) {
+        return Double.compare(o2.getValue().getPreco(), o1.getValue().getPreco());
+    }
+}
+
+class ComparatorPorAutor implements Comparator<Map.Entry<String, Livro>> {
+    @Override
+    public int compare(Map.Entry<String, Livro> o1, Map.Entry<String, Livro> o2) {
+        return o1.getValue().getAutor().compareToIgnoreCase(o2.getValue().getAutor());
+    }
+}
