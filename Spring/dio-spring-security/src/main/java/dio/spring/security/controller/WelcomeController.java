8f17051df379687f4d8508e3b3bae21bafd2a88d
PHVital <vital.ph.g@gmail.com>
2025-07-18 14:32:26 -0300

    feat: Adiciona aula segurança com Spring Security

---
+++ Spring/dio-spring-security/src/main/java/dio/spring/security/controller/WelcomeController.java
@@ -0,0 +1,23 @@
+package dio.spring.security.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome() {
+        return "Welcome to Spring Security";
+    }
+
+    @GetMapping("/users")
+    public String users() {
+        return "Authorized user";
+    }
+
+    @GetMapping("/managers")
+    public String managers() {
+        return "Authorized manager";
+    }
+}
